5b25b6b00bcf4fb72250bf26b8108609d82c6d87
iticus <ticus.ionut@gmail.com>
2018-11-12 22:03:51 +0200

    added libraries and binaries for edison (reach RS)

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ function neutis()
 {
     cp imud/libs/neutis/lib/libRTIMULib.so.8.0.0 /usr/local/lib
     cp imud/libs/neutis/bin/RTIMULibCal /usr/local/bin
-    cp imud/libs/neutis/python/RTIMU.cpython-35m-aarch64-linux-gnu.so /data/imud
     cp imud/libs/neutis/python/RTIMU.so /data/imud
 }
 
@@ -12,10 +11,10 @@ function edison()
 {
     cp imud/libs/edison/lib/libRTIMULib.so.8.0.0 /usr/local/lib
     cp imud/libs/edison/bin/RTIMULibCal /usr/local/bin
-    #TODO: python libs
+    cp imud/libs/edison/python/RTIMU.so /data/imud
 }
 
-function common()
+function pre()
 {
     echo "/usr/local/lib" >> /etc/ld.so.conf
     mkdir -p /usr/local/bin
@@ -23,24 +22,33 @@ function common()
     mkdir -p /data/imud
     cp imud/reader.py /data/imud
     cp imud/server.py /data/imud
+}
+
+function post()
+{
+    ldconfig
     cp scripts/imu_reader.service /etc/systemd/system
     cp scripts/imu_server.service /etc/systemd/system
     systemctl daemon-reload
     systemctl enable imu_reader.service
     systemctl enable imu_server.service
+    systemctl start imu_reader.service
+    systemctl start imu_server.service
 }
-
 
 case "$1" in
 "edison")
     echo "installing imud on Edison platform"
-    common
+    pre
     edison
+    post
     ;;
 *)
     echo "installing imud on Neutis platform"
-    common
+    pre
     neutis
+    post
     ;;
 esac
+
 echo "done, make sure you perform IMU calibration using RTIMULibCal"
